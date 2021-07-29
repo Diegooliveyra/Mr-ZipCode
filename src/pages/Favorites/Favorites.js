ceafe7302dca346222726ac6b53463779aa66e02
diegooliveyra <diegooliveirabryto@gmail.com>
2021-07-29 20:19:58 -0300

    corrigido erro no useLocalStorage

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -5,11 +5,11 @@ import useLocalStorage from '../../hooks/useLocalStorage';
 import * as S from './styles';
 
 const Favorites = () => {
-  const [address] = useLocalStorage('endereços');
+  const [address] = useLocalStorage('endereços', []);
 
   return (
     <S.Wrapper>
-      <Card data={address} />
+      {address ? <Card data={address} /> : <h3>Nenhum endereço salvo</h3>}
     </S.Wrapper>
   );
 };
