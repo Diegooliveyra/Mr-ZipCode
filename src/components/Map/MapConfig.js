e983447fb48d1658dfac1d517a813aa9104e9894
diegooliveyra <diegooliveirabryto@gmail.com>
2021-07-30 16:47:05 -0300

    refatorado componente map

--- src/components/Map/MapConfig.js
+++ src/components/Map/MapConfig.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useFetch from '../../hooks/useFetch';
 import { GET_GEO_LOCATION } from '../../services/api';
@@ -6,27 +6,38 @@ import Map from './Map';
 
 const MapConfig = ({ cep }) => {
   const { data, request } = useFetch();
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
   useEffect(() => {
     const { options, endpoint } = GET_GEO_LOCATION(cep);
     request(endpoint, options);
   }, [request, cep]);
-  console.log(data.results);
-  // console.log(data.results[0].geometry.location.lng);
 
-  if (data)
+  useEffect(() => {
+    if (data.results) {
+      data.results.map((item) => {
+        setLat(item.geometry.location.lat);
+        setLng(item.geometry.location.lng);
+        return null;
+      });
+    }
+  }, [data.results]);
+
+  if (data.results)
     return (
       <Map
         isMarkerShown={false}
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=KEY&v=3.exp"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=KEY_API&v=3.exp"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `300px`, width: `450px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
-        lat={-34.397}
-        lng={150.644}
-        markerLat={-34.397}
-        markerLng={50.644}
+        lat={lat}
+        lng={lng}
+        markerLat={lat}
+        markerLng={lng}
       />
     );
+  else return null;
 };
 
 export default MapConfig;
