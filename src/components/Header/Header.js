505ef898bb1a0d945012fafa9beb34ee1a4c3577
diegooliveyra <diegooliveirabryto@gmail.com>
2021-07-31 20:46:09 -0300

    corrigido link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
           </li>
           <li>
             <a
-              href="https://github.com/Diegooliveyra"
+              href="https://github.com/Diegooliveyra/Mr-Zip"
               target="_blank"
               rel="noreferrer"
             >
